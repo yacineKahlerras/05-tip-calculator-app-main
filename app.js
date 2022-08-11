4d350af8d58b6ef87265dc6bfb2afae709fd0350
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-08-11 14:50:59 +0100

    fonctioning

--- app.js
+++ app.js
@@ -1,6 +1,76 @@
 /**on each key up calculate amount
  */
 const billInput = document.getElementById("bill-input");
-const numberOfPeople = document.getElementById("number-people");
+const numberOfPeopleInput = document.getElementById("number-people");
+const customInput = document.getElementById("custom-input");
+const billErrorText = document.getElementById("bill-error");
+const numberOfPeopleErrorText = document.getElementById("number-people-error");
 const tipAmountText = document.getElementById("tip-amount");
 const totalAmountText = document.getElementById("total-amount");
+let selectedTipButton = document.querySelector(".selected-tip");
+const tipButtons = [...document.querySelectorAll(".tip-button")];
+let tipPourcentage = selectedTipButton.id / 100;
+let errorFlag = false;
+
+/** does all the calculations */
+const doCalculation = () => {
+  checkIfInputsEmpty();
+  if (errorFlag) return;
+  let bill = billInput.value;
+  let people = numberOfPeopleInput.value;
+  let tipPerPerson = (bill * tipPourcentage) / people;
+  let totalBillPerPerson = bill / people + tipPerPerson;
+  tipAmountText.textContent = tipPerPerson.toFixed(2);
+  totalAmountText.textContent = totalBillPerPerson.toFixed(2);
+};
+
+/** checks if either inputs are empty or zero */
+const checkIfInputsEmpty = () => {
+  let billValue = billInput.value;
+  let peopleValue = numberOfPeopleInput.value;
+  let billError = false,
+    peopleError = false;
+  if (billValue <= 0 || billValue == null) {
+    billErrorText.classList.add("show");
+    billError = true;
+  } else {
+    billErrorText.classList.remove("show");
+    billError = false;
+  }
+  if (peopleValue < 1 || peopleValue == null) {
+    numberOfPeopleErrorText.classList.add("show");
+    peopleError = true;
+  } else {
+    numberOfPeopleErrorText.classList.remove("show");
+    peopleError = false;
+  }
+  if (billError || peopleError) errorFlag = true;
+  else errorFlag = false;
+};
+
+/** selects a tip */
+const selectTip = (button) => {
+  selectedTipButton = document.querySelector(".selected-tip");
+  if (selectedTipButton) selectedTipButton.classList.remove("selected-tip");
+  if (button.id == "custom-tip") {
+    customInput.classList.add("show");
+    tipPourcentage = customInput.value / 100;
+    doCalculation();
+    return;
+  }
+  customInput.classList.remove("show");
+  button.classList.add("selected-tip");
+  tipPourcentage = button.id / 100;
+  doCalculation();
+};
+
+/**does a custom tip */
+const customizeTip = () => {
+  tipPourcentage = customInput.value / 100;
+  doCalculation();
+};
+
+billInput.addEventListener("keyup", doCalculation);
+numberOfPeopleInput.addEventListener("keyup", doCalculation);
+customInput.addEventListener("keyup", customizeTip);
+tipButtons.forEach((b) => b.addEventListener("click", () => selectTip(b)));
