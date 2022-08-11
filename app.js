0d863a0e6026ff66d957e8c4cb429f56fefba193
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-08-11 12:15:10 +0100

    design

---
+++ app.js
@@ -0,0 +1,6 @@
+/**on each key up calculate amount
+ */
+const billInput = document.getElementById("bill-input");
+const numberOfPeople = document.getElementById("number-people");
+const tipAmountText = document.getElementById("tip-amount");
+const totalAmountText = document.getElementById("total-amount");
